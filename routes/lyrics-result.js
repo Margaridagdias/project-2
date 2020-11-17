7c6536e692b947d6b4088288f6f5f2e60df8bf1d
Joao Uva <joaouva@gmail.com>
2020-11-17 14:12:29 +0000

    add edit functionality to user and styling to webpage

--- routes/lyrics-result.js
+++ routes/lyrics-result.js
@@ -2,27 +2,44 @@ const express = require('express');
 const router  = express.Router();
 const geniusAPI = require('genius-api');
 const genius = new geniusAPI('K5zGBdFLMx1ycnEEde2N8LnaQz3s_swt7goDvm7etnBV9x4F-h3sQ8zRyE6VGjyz');
+
+
 router.get('/lyrics', (req, res, next) => {
   res.render('lyrics-result');
 });
+
 
 router.get('/lyrics-result/:songId', (req, res) => {
   let songId = req.params.songId;
   genius.song(songId)
     .then((songFound) => {
       console.log ('testeeeeeeeee', songFound.song);
-
-     // {{!-- FALTAAAAAAAAAAAA ulnessar --}} --------------------------------------------------
-     
-      //guardar na base de dados que eu carreguei nesta song
-      History.create({artistName: '', song: ''}).then(() => {
-        res.render('lyrics-result', { songs: songFound.song});
-      })  
+      res.render('lyrics-result', { songs: songFound.song});
     })
     .catch((err) => {
       res.render('error', { err });
     })
 });
+
+
+
+// router.get('/lyrics-result/:songId', (req, res) => {
+//   let songId = req.params.songId;
+//   genius.song(songId)
+//     .then((songFound) => {
+//       console.log ('testeeeeeeeee', songFound.song);
+
+//      // {{!-- FALTAAAAAAAAAAAA ulnessar --}} --------------------------------------------------
+     
+//       //guardar na base de dados que eu carreguei nesta song
+//       History.create({artistName: '', song: ''}).then(() => {
+//         res.render('lyrics-result', { songs: songFound.song});
+//       })  
+//     })
+//     .catch((err) => {
+//       res.render('error', { err });
+//     })
+// });
 
 
 module.exports = router;
--- routes/main.js
+++ routes/main.js
@@ -1,11 +1,8 @@
 const express = require('express');
 const app = require('../app');
 const router  = express.Router();
-<<<<<<< HEAD
 const User = require('../models/User.model')
-=======
 const History = require('../models/History');
->>>>>>> 724898372e2606f1b118540257e5e692d7bb7771
 
 function requireLogin(req, res, next) {
   if (req.session.currentUser) {
